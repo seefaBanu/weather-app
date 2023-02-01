8ef7fa77e430ff1a6e30235325bc85dd680acc65
seefaBanu <114132712+seefaBanu@users.noreply.github.com>
2023-02-01 10:42:16 +0530

    created the react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {fetchWeather} from './fetchWeather'
+import './App.css'
 
-function App() {
+
+const App = () => {
+    const [query, setQuery] = React.useState('');
+    const [weather, setWeather] = React.useState({});
+
+    const search = async(e) => {
+        if (e.key === 'Enter') {
+            const data = await fetchWeather(query);
+            setWeather(data);
+            setQuery('');//clears the search bar
+        }
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-container'>
+        <input
+            type={'text'}
+            className={'search'}
+            placeholder={'Search...'}
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            onKeyPress={search}
+        />
+        {weather.main && (
+            <div className={'city'}>
+                <h2 className={'city-name'}>
+                    <span>{weather.name}</span>
+                    <sup>{weather.sys.country}</sup>
+                </h2>
+                <div className={'city-temp'}>
+                    {Math.round(weather.main.temp)}
+                    <sup>&deg;C</sup>
+                </div>
+                <div className={'info'}>
+                    <img
+                        className={'city-icon'}
+                        src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
+                        alt={weather.weather[0].description}
+                    />
+                    <p>{weather.weather[0].description}</p>
+            </div>
+            </div>
+        )}
+    
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
